71aa57076a4f37115e749c65e79e2431ed93737e
zhangye <2441886864@qq.com>
2018-07-13 14:21:36 +0800

    修改模型类中的str方法为name属性

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -5,4 +5,5 @@ class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
     
-    
+    def __str__(self):
+        return '%s' % self.uname
